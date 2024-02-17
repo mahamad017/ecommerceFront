b2f87518b592d010ba8cadfce33721f711710a37
mustafa <yabo23410@gmail.com>
2024-02-18 02:28:24 +0300

    role

--- src/components/layout/AppBar.jsx
+++ src/components/layout/AppBar.jsx
@@ -1,75 +1,73 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import styles from './layout.module.scss'
-import { useContext, useEffect, useState } from 'react';
-import { AppContext } from './Layout';
-import { Link } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from "./layout.module.scss";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "./Layout";
+import { Link } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import Api from '../../tools/api';
-import Loading from '../shared/Loading';
-import { Badge } from 'react-bootstrap';
+import Api from "../../tools/api";
+import Loading from "../shared/Loading";
+import { Badge } from "react-bootstrap";
 
 function AppBar() {
-    
-    const [cookie, setCookie] = useCookies('token')
+    const [cookie, setCookie] = useCookies("token");
     const [statistics, setStatistics] = useState([]);
-    const appContext = useContext(AppContext)
-        const [search, setSearch] = useState('')
-        const [loading, setLoading] = useState(false)
-    let token
+    const appContext = useContext(AppContext);
+    const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [request,setrequest] = useState("")
+    let token;
 
     const checkLogin = async () => {
-        token = appContext.appState.token != null ? appContext.appState.token : cookie?.token
-        if (token == null || token == '') return;
+        token = appContext.appState.token != null ? appContext.appState.token : cookie?.token;
+        if (token == null || token == "") return;
 
-        setLoading(true)
+        setLoading(true);
 
-        const res = await Api.fetch({ url: 'user', token })
+        const res = await Api.fetch({ url: "user", token });
 
         if (res != null && res.email != null) {
-            appContext.login(token, res)
+            appContext.login(token, res);
+            setrequest(res.role);
+        } else {
+            appContext.logout();
+            setCookie("token", null);
         }
-        else {
-            appContext.logout()
-            setCookie('token', null)
-        }
-                setLoading(false)
-            }
+        setLoading(false);
+    };
     useEffect(() => {
-    checkLogin()
-    getStatistics()
-    }, [])
-            
-            const onLogout = async () => {
-                setCookie('token', null)
-                
-                token = appContext.appState.token != null ? appContext.appState.token : cookie?.token
-                if (token == null) return;
-                
-                await Api.fetch({ method: 'PUT', url: 'logout', token })
-                
-                appContext.logout()
-                
-                console.log('window.location.href');
-                console.log(window.location.href);
-                window.location.href = '/login'
-                console.log(window.location.href);
-            };
-        const getStatistics = async () => {
-                    // call API
-        const response = await Api.fetch({
-            url: 'statistics'
-            });
+        checkLogin();
+        getStatistics();
+    }, []);
 
-                    // check response
-        if (response != null) 
-        console.log(response);
+    const onLogout = async () => {
+        setCookie("token", null);
+
+        token = appContext.appState.token != null ? appContext.appState.token : cookie?.token;
+        if (token == null) return;
+
+        await Api.fetch({ method: "PUT", url: "logout", token });
+
+        appContext.logout();
+
+        console.log("window.location.href");
+        console.log(window.location.href);
+        window.location.href = "/login";
+        console.log(window.location.href);
+    };
+    const getStatistics = async () => {
+        // call API
+        const response = await Api.fetch({
+            url: "statistics",
+        });
+
+        // check response
+        if (response != null) console.log(response);
         setStatistics(response.data); // update state with recevied categories
-        };
-    
+    };
 
     return (
         <Navbar expand="lg" className={styles.appbar + " " + "bg-body-tertiary"}>
@@ -96,6 +94,8 @@ function AppBar() {
                                     </>
                                 ) : (
                                     <>
+                                    {(request != "admin") ?(
+                                        <>
                                         <Link onClick={onLogout} className="nav-link mt-1">
                                             Logout
                                         </Link>
@@ -105,13 +105,18 @@ function AppBar() {
                                         <Link to="orders" className="nav-link">
                                             My Orders <Badge bg="secondary m-2">0{statistics["order"]}</Badge>
                                         </Link>
-                                        <Link to="dashboard" className="nav-link">
-                                            Users <Badge bg="secondary m-2">{statistics["user"]}</Badge>
-                                        </Link>
+                                    </> 
+                                    ):(
+                                    <>
                                         <Link to="categories" className="nav-link">
                                             CategoriesAction <Badge bg="secondary m-2">{statistics["category"]}</Badge>
                                         </Link>
+                                        <Link to="dashboard" className="nav-link">
+                                            Users <Badge bg="secondary m-2">{statistics["user"]}</Badge>
+                                        </Link>
                                     </>
+                                )}
+                                </>
                                 )}
                             </>
                         )}
@@ -140,4 +145,4 @@ function AppBar() {
         </Navbar>
     );
 }
-export  default AppBar;
+export default AppBar;
